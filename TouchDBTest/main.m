0c6414ccf9ac5937a44e5d999b83597831e4bdf1
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-05-26 12:33:19 +0200

    Initial Commit

---
+++ TouchDBTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchDBTest
+//
+//  Created by Sander Dijkhuis on 5/26/13.
+//  Copyright (c) 2013 Sander Dijkhuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
